db52a7e050edcd3e4dcd22040549ce0321b2efe2
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-10-17 16:59:39 +0300

    display recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -16,7 +16,109 @@ const timeout = function (s) {
 //bd8a8c43-7123-4d6c-ae61-dce616b99af4
 // show recipe 
 //https:forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=<insert your key>
+// Render recipe 
 
+const renderRecipe = function (recipe) {
+  const html = `
+  <figure class="recipe__fig">
+  <img src="${recipe.imageUrl}" alt="${recipe.title}" class="recipe__img" />
+  <h1 class="recipe__title">
+    <span>${recipe.title}</span>
+  </h1>
+</figure>
+
+<div class="recipe__details">
+  <div class="recipe__info">
+    <svg class="recipe__info-icon">
+      <use href="src/img/icons.svg#icon-clock"></use>
+    </svg>
+    <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+    <span class="recipe__info-text">minutes</span>
+  </div>
+  <div class="recipe__info">
+    <svg class="recipe__info-icon">
+      <use href="src/img/icons.svg#icon-users"></use>
+    </svg>
+    <span class="recipe__info-data recipe__info-data--people">4</span>
+    <span class="recipe__info-text">servings</span>
+
+    <div class="recipe__info-buttons">
+      <button class="btn--tiny btn--increase-servings">
+        <svg>
+          <use href="src/img/icons.svg#icon-minus-circle"></use>
+        </svg>
+      </button>
+      <button class="btn--tiny btn--increase-servings">
+        <svg>
+          <use href="src/img/icons.svg#icon-plus-circle"></use>
+        </svg>
+      </button>
+    </div>
+  </div>
+
+  <div class="recipe__user-generated">
+    <svg>
+      <use href="src/img/icons.svg#icon-user"></use>
+    </svg>
+  </div>
+  <button class="btn--round">
+    <svg class="">
+      <use href="src/img/icons.svg#icon-bookmark-fill"></use>
+    </svg>
+  </button>
+</div>
+
+<div class="recipe__ingredients">
+  <h2 class="heading--2">Recipe ingredients</h2>
+  <ul class="recipe__ingredient-list">
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="src/img/icons.svg#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">1000</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">g</span>
+        pasta
+      </div>
+    </li>
+
+    <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="src/img/icons.svg#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">0.5</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">cup</span>
+        ricotta cheese
+      </div>
+    </li>
+  </ul>
+</div>
+
+<div class="recipe__directions">
+  <h2 class="heading--2">How to cook it</h2>
+  <p class="recipe__directions-text">
+    This recipe was carefully designed and tested by
+    <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+    directions at their website.
+  </p>
+  <a
+    class="btn--small recipe__btn"
+    href="${recipe.sourceUrl}"
+    target="_blank"
+  >
+    <span>Directions</span>
+    <svg class="search__icon">
+      <use href="src/img/icons.svg#icon-arrow-right"></use>
+    </svg>
+  </a>
+</div>
+`;
+
+  recipeContainer.innerHTML = '';
+  recipeContainer.insertAdjacentHTML('afterbegin', html);
+
+}
 const apiKey = `bd8a8c43-7123-4d6c-ae61-dce616b99af4`;
 const showRecipe = async function () {
   try {
@@ -30,7 +132,7 @@ const showRecipe = async function () {
     // updating recipe object
     let { recipe } = data.data;
     recipe = {
-      title: recipe.id,
+      title: recipe.title,
       id: recipe.id,
       imageUrl: recipe.image_url,
       sourceUrl: recipe.source_url,
@@ -40,6 +142,8 @@ const showRecipe = async function () {
       ingredients: recipe.ingredients
     }
     console.log(recipe);
+    renderRecipe(recipe);
+
 
   } catch (err) {
     alert(err);
