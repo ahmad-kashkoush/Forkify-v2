f5a13d5d268006f31fb149de405841f6fe6a13ef
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2023-10-20 15:28:44 +0300

    pagination with no Design 💣💣

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,7 +3,7 @@ import * as model from "./model.js"
 import recipeView from './views/recipeView.js';
 import searchResultsView from './views/searchResultsView.js';
 import searchView from './views/searchView.js';
-
+import icons from "../img/icons.svg"
 
 
 // https://forkify-api.herokuapp.com/v2
@@ -34,24 +34,85 @@ const controlRecipe = async function () {
 
 }
 
+
+///////////////////////////// Pagination
+const renderPagination = function (page = 6) {
+  // parent
+  const paginationElement = document.querySelector('.pagination');
+
+  paginationElement.innerHTML = '';
+  //  generate Markup
+  const markup = `
+  ${page - 1 >= 1 ? `
+  <button button class="btn--inline pagination__btn--prev" >
+  <svg class="search__icon">
+  <use href="${icons}.svg#icon-arrow-left"></use>
+  </svg>
+  <span>Page ${page - 1}</span>
+  </button > `
+      : ''}
+  ${page + 1 <= model.state.search.lastPage ?
+      `
+  <button class="btn--inline pagination__btn--next">
+  <span>Page ${page + 1}</span>
+  <svg class="search__icon">
+  <use href="${icons}.svg#icon-arrow-right"></use>
+  </svg>
+  </button>
+  
+  `: ''}
+  `;
+  // console.log(markup);
+  //handler render
+  paginationElement.insertAdjacentHTML('beforeend', markup);
+  if (page + 1 <= model.state.search.lastPage) {
+    document.querySelector('.pagination__btn--next').addEventListener('click', function (e) {
+      e.preventDefault();
+      goToPage(model.state.search.page + 1);
+    })
+  }
+  if (page - 1 >= 1) {
+    document.querySelector('.pagination__btn--prev').addEventListener('click', function (e) {
+      e.preventDefault();
+      goToPage(model.state.search.page - 1);
+    })
+  }
+
+  console.log(paginationElement);
+}
+// this is the callback function
+const goToPage = function (page = 1) {
+  model.state.search.page = page;
+  console.log(model.getSearchResultsPerPage(page));
+  searchResultsView.render(model.getSearchResultsPerPage(page));
+  renderPagination(page);
+}
+
+
 const controlSearchResults = async function () {
   try {
     const query = searchView.getQuery();
     if (!query) return;
     searchResultsView.renderSpinner();
     await model.loadSearchResults(query);
-    // if (model.state.searchedRecipes.length === 0) put it in the render better
+    // if (model.state.search.results.length === 0) put it in the render better
     //   throw ('no query found');
-    searchResultsView.render(model.state.searchedRecipes);//
-    console.log(model.state.searchedRecipes);
+    goToPage();
+
   } catch (err) {
     searchResultsView.displayError();
-
+    console.log(err);
   }
 }
+
+
+
 // can write in global scope this is better this way
+// controlSearchResults();
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
   searchView.addHandlerSearch(controlSearchResults);
+
+
 }
 init();
